6586161801316059970f14bb52b789361ce644be
YairMoller <yairmoller@hilma.tech>
2024-11-26 12:05:13 +0200

    fix create tables

--- createTables.js
+++ createTables.js
@@ -1,34 +1,33 @@
-const mysql=require("mysql");
-const fs=require("fs/promises");
+const mysql = require("mysql");
+const fs = require("fs/promises");
 const { isUtf8 } = require("buffer");
-const path= require("path");
+const path = require("path");
 
-const con= mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"z10mz10m",
-    database:"school"
-})
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "z10mz10m",
+    database: "school",
+});
 
-con.connect(async err=>{
-    if(err)throw err
-    const filenames=await fs.readdir(path.normalize(`${__dirname}/entities`));
-    console.log('filenames: ', filenames);
-    for(let filename of filenames){
-        const data=JSON.parse(await fs.readFile(path.normalize(`${__dirname}/entities/${filename}`,{encoding:"utf8"})))
+con.connect(async (err) => {
+    if (err) throw err;
+    const filenames = await fs.readdir(path.normalize(`${__dirname}/entities`));
+    console.log("filenames: ", filenames);
+    for (let filename of filenames) {
+        const data = JSON.parse(await fs.readFile(path.normalize(`${__dirname}/entities/${filename}`, { encoding: "utf8" })));
 
         let columns = "";
-        data[Object.keys(data)[0]].forEach((element, index)=> {
-            columns += element
-            if (index !== data[Object.keys(data)[0].length -1]) columns += ","
+        data[Object.keys(data)[0]].forEach((element, index) => {
+            columns += element;
+            if (index !== data[Object.keys(data)[0]].length - 1) columns += ", ";
         });
-        
-        console.log('column: ', column);
-        
-        con.query(`CREATE TABLE ${Object.keys(data)[0]}(${columns})` ,(err, result) => {
+
+        console.log("column: ", columns);
+
+        con.query(`CREATE TABLE ${Object.keys(data)[0]}(${columns})`, (err, result) => {
             if (err) throw err;
             console.log(result);
-        })
+        });
     }
-}) 
-
+});
